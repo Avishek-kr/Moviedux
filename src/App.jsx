3a262090bda0ea24808266424e7a51fe5b8f0b58
Avishek-kr <ak251997@gmail.com>
2025-10-29 19:40:18 +0530

    header added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Header />
+      </div>
+      <footer className="footer">
+        <p className="footer">Footer content here</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <img src="logo.png" alt="moviedux" className="logo" />
+      <h2 className="app-subtitle">it's time for popcorn! Find your next movie here.</h2>
+    </div>
+  )
+}
