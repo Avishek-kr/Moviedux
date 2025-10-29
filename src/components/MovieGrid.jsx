79d61bf0ea39994fca4fbece45a7583cf449fe38
Avishek-kr <ak251997@gmail.com>
2025-10-29 21:15:02 +0530

    filtered logic added

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -4,6 +4,7 @@ import MovieCard from "./MovieCard";
 export default function MovieGrid() {
 
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch("movies.json")
@@ -11,13 +12,28 @@ export default function MovieGrid() {
     .then(data => setMovies(data))
   }, []);
 
+  const handleSearchInput = (e) => {
+    setSearchTerm(e.target.value);
+  }
+  
+  const filteredMovies = movies?.filter((movie) => movie.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()));
+
   return (
-    <div className="movies-grid">
-          {
-        movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))
-      }
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search movies..."
+        onChange={handleSearchInput}
+        value={searchTerm}
+      />
+      <div className="movies-grid">
+            {
+              filteredMovies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))
+            }
+      </div>
     </div>
   )
 }
