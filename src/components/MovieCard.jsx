ba82f26bdb1d0b7b298fe934c45afacee0e3aa5e
Avishek-kr <ak251997@gmail.com>
2025-10-29 20:57:51 +0530

    rating added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,13 +6,25 @@ export default function MovieCard({ movie }) {
     e.target.src = "/images/default.jpg";
   }
 
+  const getRatingClass = (rating) => {
+    if (rating >= 8) {
+      return "rating-good";
+    }
+
+    if (rating >= 5 && rating < 8) {
+      return "rating-ok"
+    }
+
+    return "rating-bad"
+  }
+
   return (
       <div key={movie.id} className="movie-card">
         <img src={`images/${movie.image}`} alt={movie.title} onError={handleImageError} />
         <div className="movie-card-info">
           <h3 className="movie-card-title">{movie.title}</h3>
           <div className="movie-card-genre">{movie.genre}</div>
-          <div className="movie-card-rating">{movie.rating}</div>
+          <div className={`movie-card-rating ${getRatingClass(movie.rating)}`}>{movie.rating}</div>
         </div>
       </div>
   )
